a74b99f348e594e4d7a7e7be6fc12c3b88f03ede
Marc-Olivier Bloch <wormsparty@gmail.com>
2012-09-22 17:02:07 +0200

    First version.

---
+++ README.md
@@ -0,0 +1,24 @@
+
+
+A comics reader, as simple as it should be.
+
+It can load any archive supported by libarchive.
+It can read any image format supported by pyglet.
+The window is fullscreen and it goes to the next image on any key stroke.
+
+
+-------------------
+-- Dependencies: --
+-------------------
+
+python3
+pyarchive
+pyglet
+
+
+----------
+-- TODO --
+----------
+
+Be able to go backwards.
+
---
+++ comics.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+
+import archive
+import pyglet
+import sys
+import io
+
+if len(sys.argv) == 1:
+	print("Usage: " + sys.argv[0] + " (archive)+")
+	sys.exit(1)
+
+idx = 1
+a = None
+
+def load_next_archive():
+	global idx
+	global a
+
+	if idx < len(sys.argv):
+		a = iter(archive.Archive(sys.argv[idx]))
+		idx += 1
+	else:
+		print("Done :)")
+		sys.exit(0)
+
+load_next_archive()
+
+# Initialize graphics
+window = pyglet.window.Window(fullscreen=True)
+img = None
+
+def load_next_image():
+	global img
+	global a
+
+	try:
+		element = a.__next__()
+		print("Loading " + element.filename)
+	except StopIteration:
+		load_next_archive()
+		load_next_image()
+		return
+
+	buff = element.read()	
+
+	if len(buff) > 0:
+		f = io.BytesIO(buff)
+		img = pyglet.sprite.Sprite(pyglet.image.load(element.filename, file=f))
+		img.scale = float(window.height) / float(img.image.width)
+		img.set_position(img.image.height * img.scale, 0)
+		img.rotation = -90.0
+	else:
+		load_next_image()
+
+load_next_image()
+
+@window.event
+def on_draw():
+	window.clear()
+
+	if img is not None:	
+		img.draw()
+
+@window.event
+def on_key_press(symbol, modifiers):
+	if symbol == pyglet.window.key.Q:
+		sys.exit(0)
+	else:
+		load_next_image()
+
+pyglet.app.run()
